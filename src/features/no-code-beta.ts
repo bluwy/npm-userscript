17d6e23ed7ddba5e9b492c724a8c1bcf5d23a2b2
bluwy <bjornlu.dev@gmail.com>
2026-01-25 10:20:51 +0800

    Disable no-code-beta by default

--- src/features/no-code-beta.ts
+++ src/features/no-code-beta.ts
@@ -1,8 +1,11 @@
 import { addStyle } from '../utils.ts'
 
 export const description = `\
-Hide the "Beta" label in the package code tab because it has been working for around 3 years now.
+Hide the "Beta" label in the package code tab.
 `
+
+// The code tab actually still suck after all these years, so it should still be considered beta
+export const disabled = true
 
 export function runPre() {
   addStyle(`
