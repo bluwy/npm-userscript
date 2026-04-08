8bcfc42118b3034b3ea51adcde601d56139ea0ff
bluwy <bjornlu.dev@gmail.com>
2026-04-08 11:00:02 +0800

    Remove dim mode

--- src/all-features.ts
+++ src/all-features.ts
@@ -1,6 +1,5 @@
 import * as betterDependencies from './features/better-dependencies.ts'
 import * as betterVersions from './features/better-versions.ts'
-import * as dimMode from './features/dim-mode.ts'
 import * as fixIssuePrCount from './features/fix-issue-pr-count.ts'
 import * as fixStyles from './features/fix-styles.ts'
 import * as helpfulLinks from './features/helpful-links.ts'
@@ -49,7 +48,6 @@ export interface FeatureModule {
 export const allFeatures: Record<string, FeatureModule> = {
   'better-dependencies': betterDependencies,
   'better-versions': betterVersions,
-  'dim-mode': dimMode,
   'fix-issue-pr-count': fixIssuePrCount,
   'fix-styles': fixStyles,
   'helpful-links': helpfulLinks,
